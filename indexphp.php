178bac1a32f9ba9761150689a7dfdf15bbf6e3f7
JeanerSilva <jeanerufmg@gmail.com>
2018-10-26 20:59:11 -0300

    Teste

--- indexphp.php
+++ indexphp.php
@@ -66,7 +66,8 @@
 								echo "</td><td class=\"processo4\">"; print_r($Row[2]); //objeto
 								echo "</td><td class=\"processo5\">"; print_r($Row[3]); //Requisitante
 								echo "</td><td class=\"processo7\">"; print_r($Row[6]); //subfase
-								echo "</td><td class=\"processo6\">"; print_r($Row[7]); echo "<div id=\"myProgress\"><div id=\"myBar\"></div></div>"; //fase
+								echo "</td><td class=\"processo6\">"; print_r($Row[7]);
+									echo "<div id=\"myProgress\"><div id=\"myBar\"></div></div>"; //fase
               	echo "</td><td class=\"processo2\">"; print_r($Row[4]); //modalidade
 								echo "</td><td class=\"processo3\">"; print_r($Row[0]); //responsável
 
