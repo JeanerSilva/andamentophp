655c1c14a19d006172acdc6b1f7b607fd8498e2b
JeanerSilva <jeanerufmg@gmail.com>
2018-10-30 02:20:25 -0300

    Gráfico

--- indexphp.php
+++ indexphp.php
@@ -21,26 +21,25 @@
 		<header>
 			<div class="container" id="barra">
 				<?php echo "<h2>"; echo "PROCESSOS " . strtoupper($TabelaTipo);  echo "</h2>";
-								if ($TabelaTipo == "divcon") {
-									echo "<button class=\"btn-small\" onclick=\"";
-									echo "window.location = 'indexphp.php?filtro=&tabela=divcol&oculta=false'";
-									echo "\" id=\"divcon\">Tabela DIVCOL</button>";
-								}
-									if ($TabelaTipo == "divcol") {
-									echo "<button class=\"btn-small\" onclick=\"";
-									echo "window.location = 'indexphp.php?filtro=&tabela=divcon&oculta=false'";
-									echo "\" id=\"divcon\">Tabela DIVCON</button>";
-								}
-				
-				
+					if ($TabelaTipo == "divcon") {
+						echo "<button class=\"btn-small\" onclick=\"";
+						echo "window.location = 'indexphp.php?filtro=&tabela=divcol&oculta=false'";
+						echo "\" id=\"divcon\">Tabela DIVCOL</button>";
+					}
+						if ($TabelaTipo == "divcol") {
+						echo "<button class=\"btn-small\" onclick=\"";
+						echo "window.location = 'indexphp.php?filtro=&tabela=divcon&oculta=false'";
+						echo "\" id=\"divcon\">Tabela DIVCON</button>";
+					}
+
+					
 				?>
+
 			</div>
 		</header>
 		<main>
 
-			<?php
 
-			?>
 			<form action="indexphp.php" method="get" id="menu">
 				<input type="text" name="filtro" value="<?= $Filtro ?>"
 			 id="filtrar-tabela" placeholder="Informe o termo a ser filtrado (em qualquer campo)">
@@ -55,7 +54,14 @@
 				 ?>
 			</form>
 
+			
+
 		<?php
+
+
+
+
+
 			if ($TabelaTipo == "divcol") {
         	echo "<table id=\"tabela\"><tr id=\"cabecalho\">";
         	echo "<th>Processo</th>";
@@ -64,8 +70,61 @@
         	echo "<th>Andamento</th>";
 					echo "<th>Nº </th>";
 					echo "<th>Responsável</th>";
-        	echo "</tr>";
+			echo "</tr>";
+			
+			$InstrucaoInicial = 0;
+			$ElaboracaoDeEdital = 0;
+			$DevolvidoAoRequisitante = 0;
+			$ChefiaDaDivcol = 0;
+			$EnviadoAASJUR = 0;
+			$AnalisePosASJUR = 0;
+			$EnvioACGOF = 0;
+			$Pregao = 0;
+			$InstrucaoFinal = 0;
+			$Padrao = 10;	
         	foreach ($Spreadsheet as $Key => $Row) {
+
+
+				
+				$InstrucaoInicialP = $InstrucaoInicial * $Padrao;
+				$ElaboracaoDeEditalP = $ElaboracaoDeEdital * $Padrao;
+				$DevolvidoAoRequisitanteP = $DevolvidoAoRequisitante * $Padrao;
+				$ChefiaDaDivcolP = $ChefiaDaDivcol * $Padrao;
+				$EnviadoAASJURP = $EnviadoAASJUR * $Padrao;
+				$AnalisePosASJURP = $AnalisePosASJUR * $Padrao;
+				$EnvioACGOFP = $EnvioACGOF * $Padrao;
+				$PregaoP = $Pregao * $Padrao;
+				$InstrucaoFinalP = $InstrucaoFinal * $Padrao;
+
+				echo "<div id=\"dados\" class=\"invisivel\">";
+				echo "<p class=\"bar-blue-small\">Instrução inicial: $InstrucaoInicial";
+					echo "<div class=\"bar-blue-small\" style=\"width: $InstrucaoInicialP"; 
+					echo "px;\"></div></p>";
+				echo "<p class=\"bar-blue-small\">Elaboração de edital: $ElaboracaoDeEdital";
+					echo "<div class=\"bar-blue-small\" style=\"width: $ElaboracaoDeEditalP"; 
+					echo "px;\"></div></p>";
+				echo "<p class=\"bar-blue-small\">Devolvido ao requisitante: $DevolvidoAoRequisitante";
+					echo "<div class=\"bar-blue-small\" style=\"width: $DevolvidoAoRequisitanteP"; 
+					echo "px;\"></div></p>";
+				echo "<p class=\"bar-blue-small\">Chefia da DIVCOL: $ChefiaDaDivcol";
+					echo "<div class=\"bar-blue-small\" style=\"width: $ChefiaDaDivcolP"; 
+					echo "px;\"></div></p>";
+				echo "<p class=\"bar-blue-small\">Enviado à ASJUR: $EnviadoAASJUR";
+					echo "<div class=\"bar-blue-small\" style=\"width: $EnviadoAASJURP"; 
+					echo "px;\"></div></p>";
+				echo "<p class=\"bar-blue-small\">Análise pós ASJUR: $AnalisePosASJUR";
+					echo "<div class=\"bar-blue-small\" style=\"width: $AnalisePosASJURP"; 
+					echo "px;\"></div></p>";
+				echo "<p class=\"bar-blue-small\">Enviado à CGOF: $EnvioACGOF";
+					echo "<div class=\"bar-blue-small\" style=\"width: $EnvioACGOFP"; 
+					echo "px;\"></div></p>";
+				echo "<p class=\"bar-blue-small\">Execução de Pregão: $Pregao";
+					echo "<div class=\"bar-blue-small\" style=\"width: $PregaoP"; 
+					echo "px;\"></div></p>";
+				echo "<p class=\"bar-blue-small\">Instrução final: $InstrucaoFinal";
+					echo "<div class=\"bar-blue-small\" style=\"width: $InstrucaoFinalP"; 
+					echo "px;\"></div></p>";
+					
 
 						$Concluido = strcasecmp($Row[7], "Envio à DIVCON");
 						$Arquivado = strcasecmp($Row[7], "arquivado");
@@ -74,15 +133,42 @@
 						if ($Row && $Key > 3 && strlen($Row[7]) != 0) {
 						//	$Andamentos = str_replace(";", "<br>", $Row[6]);
 							$Percentagem = "0%";
-							if (strcasecmp($Row[7], "Instrução inicial") == 0) $Percentagem = "10%";
-							if (strcasecmp($Row[7], "Elaboração de edital") == 0) $Percentagem = "20%";
-							if (strcasecmp($Row[7], "Devolvido ao requisitante") == 0) $Percentagem = "30%";
-							if (strcasecmp($Row[7], "Chefia da DIVCOL") == 0) $Percentagem = "40%";
-							if (strcasecmp($Row[7], "Enviado à ASJUR") == 0) $Percentagem = "50%";
-							if (strcasecmp($Row[7], "Análise pós ASJUR") == 0) $Percentagem = "60%";
-							if (strcasecmp($Row[7], "Envio CGOF") == 0) $Percentagem = "70%";
-							if (strcasecmp($Row[7], "Pregão") == 0) $Percentagem = "80%";
-							if (strcasecmp($Row[7], "Instrução final") == 0) $Percentagem = "90%";
+							if (strcasecmp($Row[7], "Instrução inicial") == 0) {
+								$InstrucaoInicial = $InstrucaoInicial + 1; 				
+								$Percentagem = "10%";
+							}
+							if (strcasecmp($Row[7], "Elaboração de edital") == 0) {
+								$Percentagem = "20%";
+								$ElaboracaoDeEditalP = $ElaboracaoDeEdital +1;
+							}
+							if (strcasecmp($Row[7], "Devolvido ao requisitante") == 0) {
+								$Percentagem = "30%";
+								$DevolvidoAoRequisitante = $DevolvidoAoRequisitante + 1;
+							}
+							if (strcasecmp($Row[7], "Chefia da DIVCOL") == 0) {
+								$Percentagem = "40%";
+								$ChefiaDaDivcol = $ChefiaDaDivcol +1;
+							}
+							if (strcasecmp($Row[7], "Enviado à ASJUR") == 0) {
+								$Percentagem = "50%";
+								$EnviadoAASJUR = $EnviadoAASJUR +1;
+							}
+							if (strcasecmp($Row[7], "Análise pós ASJUR") == 0) {
+								$Percentagem = "60%";
+								$AnalisePosASJUR = $AnalisePosASJUR+1;
+							}
+							if (strcasecmp($Row[7], "Envio CGOF") == 0) {
+								$Percentagem = "70%";
+								$EnvioACGOF = $EnvioACGOF + 1;
+							}
+							if (strcasecmp($Row[7], "Pregão") == 0) {
+								$Percentagem = "80%";
+								$Pregao = $Pregao +1;
+							}
+							if (strcasecmp($Row[7], "Instrução final") == 0) {
+								$Percentagem = "90%";
+								$InstrucaoFinal = $InstrucaoFinal + 1;
+							}
 							if (strcasecmp($Row[7], "Concluído") == 0 || strcasecmp($Row[7], "Envio à DIVCON") == 0) $Percentagem = "100%";
 
 								echo "<tr class =\"processos\">";
